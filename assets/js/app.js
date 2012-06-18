b9fbfb37b6a5b517348dcfb4fd4461c2691a9f63
Osman Üngür <osmanungur@gmail.com>
2012-06-18 11:09:33 +0300

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$("#bus_search_input").keypress(function (event) {
+    if (event.which == 13) {
+        event.preventDefault();
+    }
+
+});
+$("#stop_search_input").keypress(function (event) {
+    if (event.which == 13) {
+        event.preventDefault();
+    }
+
+});
