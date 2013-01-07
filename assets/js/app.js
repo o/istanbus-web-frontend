3b4403c1d8af661c0a190dfcab65b6e7710ae535
Osman Üngür <osmanungur@gmail.com>
2013-01-07 18:49:49 +0200

    StopSearch

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,36 +8,67 @@ function App () {
   var messages = {};
   
   var params = {
-    apiEndpoint: 'http://host:port',
+    apiEndpoint: 'http://178.79.173.195:8000',
   };
   
   var routes = {
     search: {
-      bus: function(id){return '/search/bus/' + id},
-      stop: function(id){return '/search/stop/' + id}
+      bus: function(id){return params.apiEndpoint + '/search/bus/' + id},
+      stop: function(id){return params.apiEndpoint + '/search/stop/' + id}
     },
     solutions: {
-      howtogo: function(from, to) {return '/howtogo/from/' + from + '/to/' +  to}
+      howtogo: function(from, to) {return params.apiEndpoint + '/howtogo/from/' + from + '/to/' +  to}
     },
     stop: {
-      detail: function(id) {return '/stop/' + id},
-      closest: function(lat, lon) {return '/closest/lat/' + lat + '/lon/' + lon}
+      detail: function(id) {return params.apiEndpoint + '/stop/' + id},
+      closest: function(lat, lon) {return params.apiEndpoint + '/closest/lat/' + lat + '/lon/' + lon}
     },
     bus: {
-      detail: function(id) {return '/bus/' + id},
-      timesheet: function(id) {return '/bus/' + id + '/timesheet'},
-      stopsGo: function(id) {return '/bus/' + id + '/stops/go'},
-      stopsCome: function(id) {return '/bus/' + id + '/stops/come'}
+      detail: function(id) {return params.apiEndpoint + '/bus/' + id},
+      timesheet: function(id) {return params.apiEndpoint + '/bus/' + id + '/timesheet'},
+      stopsGo: function(id) {return params.apiEndpoint + '/bus/' + id + '/stops/go'},
+      stopsCome: function(id) {return params.apiEndpoint + '/bus/' + id + '/stops/come'}
     }
   };
 
   this.init = function() {
     helpers.initializeTabs();
+    eventHandlers.searchStop();
   };
 
   helpers.initializeTabs = function() {
     $('nav').foundationTabs();
   }
+
+  eventHandlers.searchStop = function() {
+    $('#stopKeyword').keyup(
+      function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+        }
+        var keyword = $(this).val().trim();
+        ajaxHandlers.searchStop(keyword, domBuilders.searchStop);
+      }
+    );
+  }
+  
+  domBuilders.searchStop = function(results) {
+    if (results.length > 0) {
+      $('#wideArea').html('<div id="resultList"></div>');
+      $.each(results, function(index, val) {
+        $('#resultList').append(partialViews.stopResult(val));
+      });
+    };
+  };
+  
+  partialViews.stopResult = function(json) {
+    return '<div class="resultListElement" data-id="' + json.id + '">' + json.name + '</div>'
+  };
+
+  ajaxHandlers.searchStop = function(keyword, callback) {
+    $.get(routes.search.stop(keyword), callback);
+  };
+  
 
 }
 
