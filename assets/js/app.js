dfa42c79a81b268ca9f9249fe40df4a05d0f1d99
Osman Üngür <osmanungur@gmail.com>
2013-01-16 23:20:47 +0200

    Maps!

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,11 @@ function App () {
   var helpers = {};    
   var params = {
     apiEndpoint: 'http://178.79.173.195:8000',
+    defaultMapZoom: 15,
+    defaultMapLatLon: [41.042252,29.006889]
   };
-  
+  var map;
+  var marker;
   var routes = {
     search: {
       bus: function(id){return params.apiEndpoint + '/search/bus/' + id},
@@ -43,6 +46,7 @@ function App () {
     
   onLoadHandlers.searchStop = function() {
     ajaxHandlers.searchStop();
+    helpers.initializeMap();
   };
   
   ajaxHandlers.searchStop = function() {
@@ -82,6 +86,10 @@ function App () {
   };
   
   domBuilders.stopDetails = function(result) {
+    map.setView(result.location, params.defaultMapZoom);
+    L.Util.requestAnimFrame(map.invalidateSize, map, false, map._container);
+    marker.setLatLng(result.location);
+    $('#stopDetail table tbody').empty();    
     if (result.bus_list.length > 0) {
       $('#stopResults').slideUp();
       $('#stopDetail').fadeIn();
@@ -94,6 +102,15 @@ function App () {
 
   partialViews.stopDetails = function(bus) {
     return '<tr data-bus-id=' + bus.id + '><td>' + bus.id + '</td><td>' + bus.name + '</td></tr>';
+  };
+
+  helpers.initializeMap = function() {
+    map = L.map('map').setView(params.defaultMapLatLon, params.defaultMapZoom);
+    L.tileLayer('http://{s}.tile.cloudmade.com/5f9a0dab187a45cf8688a68cb55680a2/997/256/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }).addTo(map);
+    marker = L.marker(params.defaultMapLatLon).addTo(map);
   };
 
 }
