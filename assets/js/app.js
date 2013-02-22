63286cea1cc9d2fc77bf7df5ecd10c910059360e
Osman Üngür <osmanungur@gmail.com>
2013-02-22 10:45:22 +0200

    Added GA Minor fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -395,5 +395,15 @@ $.ajaxSetup({
   dataType: 'json'
 });
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38712116-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 var app = new App();
 app.init();
