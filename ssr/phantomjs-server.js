4d6ea117573bb86b3c6d9aa35d7e9e9b376c7577
Mustafa Paltun <mpaltun@gmail.com>
2013-06-18 00:50:18 +0300

    server side javascript rendering with phantomjs

---
+++ ssr/phantomjs-server.js
@@ -0,0 +1,37 @@
+var webpage = require('webpage');
+var server = require('webserver').create();
+
+var URL_PREFIX = "http://istanbus.org/#!";
+
+var not_found = function(response) {
+  response.statusCode = 404;
+  response.write('<html><body>Not Found!</body></html>');
+  response.close();
+}
+
+var service = server.listen(7070, function(request, response) {
+  var url = request.url;
+  var parts = url.split('_escaped_fragment_=');
+  if (parts.length > 1) {
+    var dest = URL_PREFIX + parts[1];
+    console.log("working for url: " + dest);
+
+    var page = webpage.create();
+    page.open(dest, function (status) {
+      if (status == 'success') {
+        var html = page.evaluate(function () {
+          return document.getElementsByTagName('html')[0].outerHTML;
+        });
+        response.statusCode = 200;
+        response.write(html);
+        response.close();
+      }
+      else {
+        not_found(response);
+      }
+    });
+  }
+  else {
+    not_found(response);
+  }
+});
