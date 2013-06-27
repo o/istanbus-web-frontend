587ebe8618f47b4e14d950260fd870044e86bf92
Mustafa Paltun <mpaltun@gmail.com>
2013-06-27 09:37:20 +0300

    fix ga

--- app/js/modules.js
+++ app/js/modules.js
@@ -186,22 +186,25 @@ providers.provider('routeService', function() {
 });
 
 providers.provider('GA', function () {
-  this.$get = function() {
+  this.$get = function($window, $document) {
     return {
       init: function() {
         if (navigator.userAgent == "istanbusSSR") {
           // ignore istanbus server side renderer
           return;
         }
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-38712116-1']);
-        _gaq.push(['_trackPageview']);
+        
+        $window._gaq = [];
+        $window._gaq.push(['_setAccount', 'UA-38712116-1']);
+        $window._gaq.push(['_trackPageview']);
 
         (function() {
+          var document = $document[0];
           var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
           ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
+        console.log('ga initialized');
       }
     }
   }
